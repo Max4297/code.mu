f897cc9dd94db0cb6b17692698c172599b07db65
Levanov Maxim <levanov.max4297@gmail.com>
2023-04-30 23:42:16 +0300

    added solutions for level 2.3, tasks 1-4

--- javaScript puzzle by levels/Level 2/level2.3.js
+++ javaScript puzzle by levels/Level 2/level2.3.js
@@ -4,11 +4,24 @@ Level 2.3
 Даны два слова. Проверьте, что последняя буква первого слова совпадает с первой буквой второго слова.
 Two words are given. Check that the last letter of the first word matches the first letter of the second word.
 */
+((word1, word2) => {
+  console.log(word1[0] === word2[0]);
+})("hello", "hello");
 /*
 #2
 Дана некоторая строка. Найдите позицию третьего нуля в строке.
 Given a string. Find the position of the third zero in the string.
 */
+((string) => {
+  let array = string.split("");
+  let count = 0;
+  for (let i = 0; i < 3; i++) {
+    let index = array.indexOf("0") + 1;
+    count += index;
+    array.splice(0, index);
+  }
+  console.log(count);
+})("Giv0n numbers se0arated by co0mas:");
 /*
 No. 3
 Даны числа, разделенные запятыми:
@@ -20,6 +33,9 @@ Given numbers separated by commas:
 '12,34,56'
 Find the sum of these numbers.
 */
+((string) => {
+  console.log(string.split(",").reduce((a, b) => a + +b, 0));
+})("12,34,56");
 /*
 #4
 Дана дата в следующем формате:
@@ -43,3 +59,11 @@ month: '12'
 day: '31'
 }
 */
+((string) => {
+  let array = string.split("-");
+  let date = {};
+  date.year = array[0];
+  date.month = array[1];
+  date.day = array[2];
+  console.log(date);
+})("2025-12-31");
