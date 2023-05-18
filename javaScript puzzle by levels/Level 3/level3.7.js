137b46bb02caf040997d3f286e260a7c01d1de9c
Levanov Maxim <levanov.max4297@gmail.com>
2023-05-18 22:03:40 +0300

    added solution for level 3.7

---
+++ javaScript puzzle by levels/Level 3/level3.7.js
@@ -0,0 +1,105 @@
+/*
+Level 3.7 of the JavaScript problem book
+#1
+Given a line with words. 
+Sort the words alphabetically.
+Дана строка со словами. 
+Отсортируйте слова в алфавитном порядке.
+*/
+((string) => {
+  console.log(string.split(" ").sort().join(" "));
+})("b word how a you");
+/*
+#2
+Given a number. Get an array of divisors of this number.
+Дано число. Получите массив делителей этого числа.
+*/
+((num) => {
+  let array = [];
+  for (let i = num; i >= 1; i--) {
+    if (num % i == 0) {
+      array.push(i);
+    }
+  }
+  console.log(array);
+})(40);
+/*
+№3
+Two numbers are given. Get an array of common divisors of these numbers.
+Даны два числа. Получите массив общих делителей этих чисел.
+*/
+((num1, num2) => {
+  let min = Math.min(num1, num2);
+  let array = [];
+  for (let i = min; i >= 1; i--) {
+    if (num1 % i === 0 && num2 % i === 0) {
+      array.push(i);
+    }
+  }
+  console.log(array);
+})(9, 21);
+/*
+#4
+Given a number. Check that this number has only one divisor besides itself and one.
+Дано число. Проверьте, что у этого числа есть только один делитель, кроме него самого и единицы.
+*/
+((num) => {
+  let array = [];
+  for (let i = num; i >= 1; i--) {
+    if (num % i == 0) {
+      array.push(i);
+    }
+  }
+  console.log(array);
+  if (array.length < 3 || array.length > 3) {
+    console.log(
+      "У числа делителей больше одного или меньше одного, кроме числа 1 и самого себя"
+    );
+  } else {
+    console.log("У числа только один делитель кроме 1 и самого себя");
+  }
+})(9);
+/*
+#5
+Numbers are written separated by commas. Get the maximum of these numbers.
+Через запятую написаны числа. Получите максимальное из этих чисел.
+*/
+/*
+#6
+Given an array of numbers. After each single-digit number, insert another one of the same.
+Дан массив с числами. После каждого однозначного числа вставьте еще такое же.
+*/
+/*
+#7
+Given a line. Remove all vowels from it.
+Дана строка. Удалите из нее все гласные буквы.
+*/
+/*
+#8
+Given a line. Capitalize the last letter of every word on this line.
+Дана строка. Сделайте заглавной последнюю букву каждого слова в этой строке.
+*/
+/*
+#9
+Given the following structure:
+Find the sum of the elements of this structure.
+Дан следующая структура:
+Найдите сумму элементов этой структуры.
+let data = [
+	{
+		1: [1, 2, 3],
+		2: [1, 2, 3],
+		3: [1, 2, 3],
+	},
+	{
+		1: [1, 2, 3],
+		2: [1, 2, 3],
+		3: [1, 2, 3],
+	},
+	{
+		1: [1, 2, 3],
+		2: [1, 2, 3],
+		3: [1, 2, 3],
+	},
+];
+*/
