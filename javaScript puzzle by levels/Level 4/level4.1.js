0eabbcd7b7a2b5d3f4555289a7187e33e9340d19
Levanov Maxim <levanov.max4297@gmail.com>
2023-05-31 00:54:15 +0300

    added solution for level 4.1 task 5

--- javaScript puzzle by levels/Level 4/level4.1.js
+++ javaScript puzzle by levels/Level 4/level4.1.js
@@ -60,6 +60,61 @@ Make a function that will take a number and a string as a parameter and trim thi
 Make a function that will receive a date as a parameter, and return the zodiac sign corresponding to this date.
 Сделайте функцию, которая параметром будет получать дату, а возвращать знак зодиака, соответствующий этой дате.
 */
+((clientDate) => {
+  let date = new Date(clientDate);
+  let day = date.getDate();
+  let month = date.getMonth() + 1;
+  if (day < 10) {
+    day = "0" + day;
+  }
+  if (month < 10) {
+    month = "0" + month;
+  }
+
+  date = month + "." + day;
+
+  console.log(date);
+
+  if (date >= "03.21" && date <= "04.19") {
+    console.log("Aries");
+  }
+  if (date >= "04.20" && date <= "05.20") {
+    console.log("Taurus");
+  }
+  if (date >= "05.21" && date <= "06.20") {
+    console.log("Gemini");
+  }
+  if (date >= "06.21" && date <= "07.22") {
+    console.log("Cancer");
+  }
+  if (date >= "07.23" && date <= "08.22") {
+    console.log("Leo");
+  }
+  if (date >= "08.23" && date <= "09.22") {
+    console.log("Virgo");
+  }
+  if (date >= "09.23" && date <= "10.22") {
+    console.log("Libra");
+  }
+  if (date >= "10.23" && date <= "11.21") {
+    console.log("Scorpio");
+  }
+  if (date >= "11.22" && date <= "12.31") {
+    console.log("Sagittarius");
+  }
+  if (date >= "01.01" && date <= "01.19") {
+    console.log("Sagittarius");
+  }
+  if (date >= "12.22" && date <= "01.19") {
+    console.log("Capricorn");
+  }
+  if (date >= "01.20" && date <= "02.18") {
+    console.log("Aquarius");
+  }
+  if (date >= "02.19" && date <= "03.20") {
+    console.log("Pisces");
+  }
+})("1997, 10, 17");
 /*
 #6
 Make a function that takes a number as a parameter and returns the sum of its divisors.
