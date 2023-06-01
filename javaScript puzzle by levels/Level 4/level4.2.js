54bc011722ae8080f5d53ecefef993eb9a87b0ac
Levanov Maxim <levanov.max4297@gmail.com>
2023-06-01 22:59:53 +0300

    added solution for level 4.2 tasks 4-7

--- javaScript puzzle by levels/Level 4/level4.2.js
+++ javaScript puzzle by levels/Level 4/level4.2.js
@@ -36,16 +36,40 @@ Make a function that will return how many days have passed or are left until a g
 Make a function that takes a year as a parameter and checks if it is a leap year or not.
 Сделайте функцию, которая параметром будет год и проверять, високосный он или нет.
 */
+((year) => {
+  let date = new Date(year + "-02-29");
+  console.log(date.getDate() == 29 ? "leap year" : "Not a leap year");
+})("2020");
 /*
 #5
 Make a function that will return an array of all leap years in the previous hundred years.
 Сделайте функцию, которая вернет массив всех високосных годов за предыдущие сто лет.
 */
+(() => {
+  let array = [];
+  for (let i = 1923; i <= 2023; i++) {
+    let date = new Date(i + "-02-29");
+    if (date.getDate() == 29) {
+      array.push(i);
+    }
+  }
+  console.log(array);
+})();
 /*
 #6
 Make a function that will return how many days are left until the end of the current month.
 Сделайте функцию, которая будет возвращать сколько дней осталось до конца текущего месяца.
 */
+(() => {
+  let currentDate = new Date();
+  let lastDayOfMonth = new Date(
+    currentDate.getFullYear(),
+    currentDate.getMonth() + 1,
+    0
+  );
+  let daysToEndOfMonth = lastDayOfMonth.getDate() - currentDate.getDate() + 1;
+  console.log(daysToEndOfMonth);
+})();
 /*
 #7
 Make a function that returns the previous, current and next days of the week as a word as the following object:
@@ -56,3 +80,25 @@ curr: 'sun',
 prev: 'Sat',
 }
 */
+(() => {
+  let date = new Date("2023, 06, 5");
+  let day = date.getDay();
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  let res = {
+    next: days[day + 1] == undefined ? days[0] : days[day + 1],
+    curr: days[day],
+    prev: days[day - 1] == undefined ? days[6] : days[day - 1],
+  };
+
+  console.log(res);
+})();
